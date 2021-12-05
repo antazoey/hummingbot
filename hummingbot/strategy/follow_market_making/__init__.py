a9718d60cb69d19a058fd1ad5201be53b9e13723
unparalleled-js <yingthi@live.com>
2021-12-05 15:50:01 -0600

    init

---
+++ hummingbot/strategy/follow_market_making/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from .follow_market_making import FollowMarketMakingStrategy
+from .inventory_cost_price_delegate import InventoryCostPriceDelegate
+__all__ = [
+    FollowMarketMakingStrategy,
+    InventoryCostPriceDelegate,
+]
